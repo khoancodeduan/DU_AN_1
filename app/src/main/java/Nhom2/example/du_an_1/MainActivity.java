bb6b402b7a60686124fe370ff2c3ba40b9bd90a0
KHOANUBU <khuongkhoan@gmail.com>
2022-11-05 15:07:14 +0700

    Initial commit

---
+++ app/src/main/java/Nhom2/example/du_an_1/MainActivity.java
@@ -0,0 +1,14 @@
+package Nhom2.example.du_an_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
